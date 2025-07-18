6c594de418bb5626e3fb908d43a17a983522ef74
charlynazzal <charlynazzalofficial@gmail.com>
2025-07-18 14:04:58 +0400

    Remove unnecessary files - keep only vehicle_parameters.py and README

--- exercise.py
+++
@@ -1,8 +0,0 @@
-# this program is to ask for your name and age
-
-print('Hello what is your name?')
-myName= input()
-print('Nice to meet you ,'+ myName)
-print('How old are you?')
-myAge = input()
-print('You will be ' + str(int(myAge)+1) + ' next year')
